684cb5c2eb16328c2d8c3806e28eb34f07dae498
Theodore <greytrt@gmail.com>
2021-08-04 14:51:37 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,39 +4,33 @@ terraform {
       source  = "kreuzwerker/docker"
       version = "2.13.0"
     }
+#   ansible = {
+#     source = "cloudposse/ansible/null"
+#     version = "0.6.0"
+#   }
   }
 }
 
 provider "docker" {
-  host = "unix:///var/run/docker.sock"
+#  host = "unix:///var/run/docker.sock"
 }
 
-# Pulls the image
-
-resource "docker_image" "ansible" {
-  # Ansible image for Ubuntu x86
-  #  name = "ansible/ansible:ubuntu1404"
-  # Ansible image for aarch64
-  name = "linoliveira/ansible-aarch64"
-  keep_locally = true
-}
-
-# Create a container
-resource "docker_container" "tensorflow" {
-  image = docker_image.ansible.latest
-  name  = "tensorflow"
-
-  ports {
-    external = 8080
-    internal = 80
+resource "docker_image" "jenkins-master" {
+  name = "jenkins-master"
+  build {
+    path = "/home/theogrey/tvm-terraform"
+    tag = ["jenkins-master:develop"]
+    build_arg = {
+      foo : "jenkins-master"
+                }
+    label = {
+       author : "theo"
+    }
   }
-
 }
 
-module "ansible_provisioner" {
- source = "github.com/cloudposse/tf_ansible"
-  arguments = ["--user=ubuntu"]
-  envs = ["host=docker_container.tensorflow"]
-  playbook = "../datacentre/ansible/build_tensorflow.yml"
-  dry_run = false
+
+resource "docker_container" "jenkins-node" {
+  name = "jenkins-node"
+  image = docker_image.jenkins-master.latest
 }
